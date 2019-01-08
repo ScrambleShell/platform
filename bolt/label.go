ea3a9907d9c0e343b789c1bbc070cce79fc8dcf1
Jade McGough <jade@thezets.com>
2019-01-08 11:39:52 -0800

    fix test

--- bolt/label.go
+++ bolt/label.go
@@ -218,7 +218,11 @@ func (c *Client) updateLabel(ctx context.Context, tx *bolt.Tx, l *platform.Label
 
 	label := ls[0]
 
-	if label.Properties != nil {
+	if label.Properties == nil {
+		label.Properties = make(map[string]string)
+	}
+
+	if upd.Properties != nil {
 		for k, v := range upd.Properties {
 			if v == "" {
 				delete(label.Properties, k)
